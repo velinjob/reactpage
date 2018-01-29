c1121104bbd2e44e9211b8c40ac810b4b51aa755
apogoreliy <apogoreliy@gmail.com>
2018-01-29 14:57:58 +0200

    Added a new page-component and a new way to apply styles to a the project

--- app/components/MembersPage.js
+++ app/components/MembersPage.js
@@ -1,19 +1,32 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import Button from './common/Button';
+import '../../styles/members.css';
 
 class MembersPage extends Component {
     componentDidMount() {
     	this.props.getMembersList();
+        this.handleBtnAddMember = this.handleBtnAddMember.bind(this);
+    }
+
+    handleBtnAddMember(){
+        console.log('test');
+
     }
 
     render() {
         return (
         	<div className="container">
-				{ 
-                    this.props.members.map(item=>{
-                        return <div key={item.id}>{item.name}</div>
-                    })
-                }
+                <div className="btn-toolbox">
+                    <Button classSet="btn-success" handleClick={this.handleBtnAddMember} text="Добавить" />
+                </div>
+                <div>
+    				{ 
+                        this.props.members.map(item=>{
+                            return <div className="test-member" key={item.id}>{item.name}</div>
+                        })
+                    }
+                </div>
 		    </div>
         )
     }
